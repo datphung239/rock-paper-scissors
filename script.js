923d518dfbddf6cfdd1ef98cf13cdfc09891090c
datphung <datphung239@gmail.com>
2023-03-09 15:54:57 +0700

    Done rock scissors paper game

--- script.js
+++ script.js
@@ -6,11 +6,10 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection,computerSelection) {
-    playerSelection = playerSelection.toLowerCase()
     if (playerSelection === computerSelection) {
         return "Draw!"
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You Lose! Paper beats Rock"
+        return `You Lose! Paper beats Rock`
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
         return "You Win! Rock breaks Scissors"
     } else if (playerSelection === "paper" && computerSelection === "rock") {
@@ -24,22 +23,34 @@ function playRound(playerSelection,computerSelection) {
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection))
+let result = document.querySelector("#result")
+let playerScore = document.querySelector("#player-score")
+let computerScore = document.querySelector("#computer-score")
+const inputs = document.querySelectorAll("input")
 
-// function game() {
-//     result = {}
-//     for (i=1;i<=5;i++) {
-//         if (playRound(playerSelection,computerSelection).includes("Win")) {
-//             result["you"]++
-//         } else if (playRound(playerSelection,computerSelection).includes("Lose")) {
-//             result["computer"]++
-//         } else {
-//             result["you"]++
-//             result["computer"]++
-//         }
-//     }
-//     return result
-// }
+let playerScoreVal = 0
+let computerScoreVal = 0
+function playRoundHTML (input) {
+    input.addEventListener("click",() => {
+        let playerSelection = input.value
+        const computerSelection = getComputerChoice()
+        let roundPlay = playRound(playerSelection,computerSelection)
+        if (roundPlay.includes("Win")) {
+            playerScoreVal++
+        } else if (roundPlay.includes("Lose")) {
+            computerScoreVal++
+            result.textContent = roundPlay
+        } else {
+            playerScoreVal++
+            computerScoreVal++
+        }
+        result.textContent = roundPlay
+        playerScore.textContent = `Player Score: ${playerScoreVal}`
+        computerScore.textContent = `Computer Score: ${computerScoreVal}`
+    })
+}
+for (i=0; i <inputs.length; i++) {
+    playRoundHTML(inputs[i])
+}
+
 
