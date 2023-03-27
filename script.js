e9be88719a2afb131f60b712caddc08ed3cc4aac
datph <datphung239@gmail.com>
2023-03-27 23:05:44 +0700

    Change indent from 4 to 2

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 // Create function for game
 function getComputerChoice() {
-    let choice = ["rock","paper","scissors"]
-    random = Math.floor(Math.random()*3)
-    randomChoice = choice[random]
-    return randomChoice
+  let choice = ["rock", "paper", "scissors"];
+  random = Math.floor(Math.random() * 3);
+  randomChoice = choice[random];
+  return randomChoice;
 }
 
-function playRound(playerSelection,computerSelection) {
-    if (playerSelection === computerSelection) {
-        return "Draw!"
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return `You Lose! Paper beats Rock`
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You Win! Rock breaks Scissors"
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You Win! Paper beats Rock"
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You Lose! Scissors cuts Paper"
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You Lose! Rock breaks Scissors"
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You Win! Scissors cuts Paper"
-    }
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "Draw!";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return `You Lose! Paper beats Rock`;
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You Win! Rock breaks Scissors";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You Win! Paper beats Rock";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You Lose! Scissors cuts Paper";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "You Lose! Rock breaks Scissors";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You Win! Scissors cuts Paper";
+  }
 }
 /*
 
@@ -116,84 +116,88 @@ function playAgain(){
 
 // Solution 2
 
-let result = document.querySelector("#result")
-let playerScore = document.querySelector("#player-score")
-let computerScore = document.querySelector("#computer-score")
-let inputsPlayer = document.querySelector(".player")
+let result = document.querySelector("#result");
+let playerScore = document.querySelector("#player-score");
+let computerScore = document.querySelector("#computer-score");
+let inputsPlayer = document.querySelector(".player");
 
-let playerScoreVal = 0
-let computerScoreVal = 0
+let playerScoreVal = 0;
+let computerScoreVal = 0;
 
-inputsPlayer.addEventListener("click",execute = (click)=> {
-    if (!click["srcElement"].value) return
+inputsPlayer.addEventListener(
+  "click",
+  (execute = (click) => {
+    if (!click["srcElement"].value) return;
     // Reset color
-    styledElement = document.querySelectorAll('.container *[style]')
-    styledElement.forEach((e) => e.removeAttribute("style"))
+    styledElement = document.querySelectorAll(".container *[style]");
+    styledElement.forEach((e) => e.removeAttribute("style"));
     // Set variables
-    const inputPlayer = click["srcElement"]
-    const playerSelection = inputPlayer.value
-    const computerSelection = getComputerChoice()
-    const inputComputer = document.querySelector(`.computer input[value="${computerSelection}"]`)
-    const roundPlay = playRound(playerSelection,computerSelection)
+    const inputPlayer = click["srcElement"];
+    const playerSelection = inputPlayer.value;
+    const computerSelection = getComputerChoice();
+    const inputComputer = document.querySelector(
+      `.computer input[value="${computerSelection}"]`
+    );
+    const roundPlay = playRound(playerSelection, computerSelection);
     // Play game
     if (roundPlay.includes("Win")) {
-        playerScoreVal++
-        inputPlayer.style.backgroundColor = "lime"
-        inputComputer.style.backgroundColor = "red"
+      playerScoreVal++;
+      inputPlayer.style.backgroundColor = "lime";
+      inputComputer.style.backgroundColor = "red";
     } else if (roundPlay.includes("Lose")) {
-        computerScoreVal++
-        inputPlayer.style.backgroundColor = "red"
-        inputComputer.style.backgroundColor = "lime"
+      computerScoreVal++;
+      inputPlayer.style.backgroundColor = "red";
+      inputComputer.style.backgroundColor = "lime";
     } else {
-        playerScoreVal++
-        computerScoreVal++
-        inputPlayer.style.backgroundColor = "yellow"
-        inputComputer.style.backgroundColor = "yellow"
+      playerScoreVal++;
+      computerScoreVal++;
+      inputPlayer.style.backgroundColor = "yellow";
+      inputComputer.style.backgroundColor = "yellow";
     }
-    result.textContent = roundPlay
-    playerScore.textContent = `Player Score: ${playerScoreVal}`
-    computerScore.textContent = `Computer Score: ${computerScoreVal}`
-    
+    result.textContent = roundPlay;
+    playerScore.textContent = `Player Score: ${playerScoreVal}`;
+    computerScore.textContent = `Computer Score: ${computerScoreVal}`;
+
     // Play again
     if (playerScoreVal === 5 || computerScoreVal === 5) {
-        playAgain()
+      playAgain();
     }
-})
+  })
+);
 
-function playAgain(){
-    // Change status color, disable button after game finish
-    if (playerScoreVal === computerScoreVal) {
-        result.textContent = `Tie !!! ${playerScoreVal}:${computerScoreVal}`
-        result.style.color = "yellow"
-        result.style.fontWeight="bold" 
-    } else if (playerScoreVal > computerScoreVal) {
-        result.textContent = `You win !!! ${playerScoreVal}:${computerScoreVal}`
-        result.style.color = "lime"
-        result.style.fontWeight="bold" 
-    } else {
-        result.textContent = `You lose !!! ${playerScoreVal}:${computerScoreVal}`
-        result.style.color = "red"
-        result.style.fontWeight="bold" 
-    }
-    inputsPlayer.querySelectorAll("input").forEach((input) => {
-        input.disabled=true
-    })
-    // Restart game from the begin
-    const playAgainButton = document.createElement("button")
-    playAgainButton.classList.add("play-again")
-    playAgainButton.textContent = "You want to play again?";
-    document.body.appendChild(playAgainButton);
+function playAgain() {
+  // Change status color, disable button after game finish
+  if (playerScoreVal === computerScoreVal) {
+    result.textContent = `Tie !!! ${playerScoreVal}:${computerScoreVal}`;
+    result.style.color = "yellow";
+    result.style.fontWeight = "bold";
+  } else if (playerScoreVal > computerScoreVal) {
+    result.textContent = `You win !!! ${playerScoreVal}:${computerScoreVal}`;
+    result.style.color = "lime";
+    result.style.fontWeight = "bold";
+  } else {
+    result.textContent = `You lose !!! ${playerScoreVal}:${computerScoreVal}`;
+    result.style.color = "red";
+    result.style.fontWeight = "bold";
+  }
+  inputsPlayer.querySelectorAll("input").forEach((input) => {
+    input.disabled = true;
+  });
+  // Restart game from the begin
+  const playAgainButton = document.createElement("button");
+  playAgainButton.classList.add("play-again");
+  playAgainButton.textContent = "You want to play again?";
+  document.body.appendChild(playAgainButton);
 
-    playAgainButton.addEventListener("click",()=>{
-        playAgainButton.remove()
-        result.textContent = "Please select your choice!"
-        result.style.color = "white"
-        playerScore.textContent = `Player Score: ${playerScoreVal = 0}`
-        computerScore.textContent = `Computer Score: ${computerScoreVal = 0}`
-        document.querySelectorAll("input").forEach((eachInput) => {
-            eachInput.removeAttribute("style")
-            eachInput.disabled=false
-        })
-    })
+  playAgainButton.addEventListener("click", () => {
+    playAgainButton.remove();
+    result.textContent = "Please select your choice!";
+    result.style.color = "white";
+    playerScore.textContent = `Player Score: ${(playerScoreVal = 0)}`;
+    computerScore.textContent = `Computer Score: ${(computerScoreVal = 0)}`;
+    document.querySelectorAll("input").forEach((eachInput) => {
+      eachInput.removeAttribute("style");
+      eachInput.disabled = false;
+    });
+  });
 }
-
